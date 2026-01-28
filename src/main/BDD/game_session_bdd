6652779cfcd27c5d6809757f6f25fd9b923c3c87
Chloe Hervouet <chloe.dp.h@hotmail.fr>
2026-01-28 16:07:18 +0100

    maj bdd

---
+++ src/main/BDD/game_session_bdd
@@ -0,0 +1,7 @@
+CREATE TABLE `game_session` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `user_id` INT NOT NULL,
+    `api_token` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`user_id`) REFERENCES `user`(`id`)
+);
--- src/main/BDD/win_session_bdd.sql
+++ src/main/BDD/win_session_bdd.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `win_session` (
                                `id` INT NOT NULL AUTO_INCREMENT,
                                `win_id` INT NOT NULL,
-                               `session_id` INT NOT NULL,
+                               `game_session_id` INT NOT NULL,
                                PRIMARY KEY (`id`),
                                FOREIGN KEY (`win_id`) REFERENCES `win`(`id`),
-                               FOREIGN KEY (`session_id`) REFERENCES `session`(`id`)
+                               FOREIGN KEY (`game_session_id`) REFERENCES `game_session`(`id`)
 )
