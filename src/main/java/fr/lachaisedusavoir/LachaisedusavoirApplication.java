d920174bf663d038dc93ba37f094c0282d54c9a6
Ota <q.tonellato@free.fr>
2026-01-28 11:46:12 +0100

    Init Spring & Maven

---
+++ src/main/java/fr/lachaisedusavoir/LachaisedusavoirApplication.java
@@ -0,0 +1,13 @@
+package fr.lachaisedusavoir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LachaisedusavoirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LachaisedusavoirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/lachaisedusavoir/LachaisedusavoirApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.lachaisedusavoir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LachaisedusavoirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
